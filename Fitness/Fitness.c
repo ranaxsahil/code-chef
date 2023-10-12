ae98391cae77776bc0f3872abd1a013c37c75ee3
yosahildon <yosahildon@gamil.com>
2023-10-12 23:07:22 +0530

    fitness problem solved!

---
+++ Fitness/Fitness.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int q;
+    scanf("%d", &q);
+    for( int i =1; i <=q ; i++)
+{
+    int n;
+    scanf("%d", &n);
+    int b = n *2 *5;
+    printf("%d", b);
+    return 0;
+}
+}
